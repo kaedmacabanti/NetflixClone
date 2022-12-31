b4df98cf1c80b81595f017b4ddeb898d83c02ae7
kaedmacabanti20 <kaedmacabanti.student@ua.edu.ph>
2022-12-31 12:01:31 +0800

    deployment test

--- my-app/pages/_document.js
+++ my-app/pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head >
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"></link>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
